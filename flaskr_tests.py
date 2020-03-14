9cf61a99af0a5dfb4982110303015d32397ceed3
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-03-14 18:54:08 +0900

    python3 flaskr_tests.py

---
+++ flaskr_tests.py
@@ -0,0 +1,12 @@
+import os
+import flaskapp
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("test")
+
+if __name__ == '__main__':
+    unittest.main()
