62039f18216326eea8071335efae2c0aa877e0c7
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-03-18 22:10:45 +0900

    generate wif

--- flaskapp/bip32test/bip32test.py
+++ flaskapp/bip32test/bip32test.py
@@ -4,43 +4,56 @@ import pprint
 import binascii
 import mnemonic
 import bip32utils
+BIP32_HARDEN    = 0x80000000 # choose from hardened set of child keys
+#http://pydoc.net/bip32utils/0.3.post3/bip32utils.BIP32Key/
 
 def bip39(mnemonic_words):
     mobj = mnemonic.Mnemonic("english")
     seed = mobj.to_seed(mnemonic_words)
     #print("seed:" + seed)
-    bip32_root_key_obj = bip32utils.BIP32Key.fromEntropy(seed, testnet=True)
-    #print("bip32_root_key_obj:" + bip32_root_key_obj)
-    bip32_child_key_obj = bip32_root_key_obj.ChildKey(
-        44 + bip32utils.BIP32_HARDEN
-    ).ChildKey(
-        0 + bip32utils.BIP32_HARDEN
-    ).ChildKey(
-        0 + bip32utils.BIP32_HARDEN
-    ).ChildKey(0).ChildKey(0)
+    m = bip32utils.BIP32Key.fromEntropy(seed, testnet=True)
+    print("* [Chain m]")
+    m.dump()
 
-    # return {
-    #     'mnemonic_words': mnemonic_words,
-    #     'bip32_root_key': bip32_root_key_obj.ExtendedKey(),
-    #     'bip32_extended_private_key': bip32_child_key_obj.ExtendedKey(),
-    #     'bip32_derivation_path': "m/44'/0'/0'/0",
-    #     'bip32_derivation_addr': bip32_child_key_obj.Address(),
-    #     'coin': 'BTC'
-    # }
+    print("* [Chain m/0h]")
+    m = m.ChildKey(0+BIP32_HARDEN)
+    m.dump()
+
+    print("* [Chain m/0h/1]")
+    m = m.ChildKey(1)
+    m.dump()
+    
+    print("* [Chain m/0h/1/2h]")
+    m = m.ChildKey(2+BIP32_HARDEN)
+    m.dump()
+
+    print("* [Chain m/0h/1/2h/2]")
+    m = m.ChildKey(2)
+    m.dump()
+
+    print("* [Chain m/0h/1/2h/2/1000000000]")
+    m = m.ChildKey(1000000000)
+    m.dump()
+
 
     return {
         'mnemonic_words': mnemonic_words,
         # 'bip32_root_key': bip32_root_key_obj.ExtendedKey(),
         # 'bip32_extended_private_key': bip32_child_key_obj.ExtendedKey(),
         # 'path': "m/44'/0'/0'/0",
-        'addr': bip32_child_key_obj.Address(),
-        'publickey': binascii.hexlify(bip32_child_key_obj.PublicKey()).decode(),
-        'privatekey': bip32_child_key_obj.WalletImportFormat(),
+        'addr': m.Address(),
+        'publickey': binascii.hexlify(m.PublicKey()).decode(),
+        'privatekey': m.WalletImportFormat(),
         'coin': 'BTC'
     }
 
+import yaml
 
 if __name__ == '__main__':
     #mnemonic_words = "aware report movie exile buyer drum poverty supreme gym oppose float elegant"
-    mnemonic_words = ""
+
+    with open('./flaskapp/bip32test/config.yaml', 'r') as yml:
+        config = yaml.load(yml)
+        
+    mnemonic_words = config["testnet"]["mnemonic"]
     pprint.pprint(bip39(mnemonic_words))
--- flaskapp/bip39mnemonic/bip39mnemonic.py
+++ flaskapp/bip39mnemonic/bip39mnemonic.py
@@ -1,44 +1,27 @@
 # https://github.com/trezor/python-mnemonic
 
 import mnemonic ##pip3 install mnemonic
-from bsvbip32 import Bip32
 import hashlib
 import hmac
 import sys
+from pycoin import key
 
 class Bip39Mnemonic(object):
-    def __init__(self, bsvmnemonic, passphrase="", network = 'main'):
-        seed = mnemonic.Mnemonic.to_seed(bsvmnemonic, passphrase)
+    def __init__(self, mnemonic_words, passphrase="", network = 'main'):
+        seed = mnemonic.Mnemonic.to_seed(mnemonic_words, passphrase)
         #self.masterkey = mnemonic.Mnemonic.to_hd_master_key(seed)
-        self.masterkey = self.to_hd_master_key(seed, network)
-        print(self.masterkey)
-        tprv = Bip32(self.masterkey)
-        self.privatekey_wif = tprv.wif()  ## it is privatekey wif format.
 
+        print(seed)
+        print(network)
+        self.extended_key = self.to_hd_master_key(seed, network == 'test')
+        print(self.extended_key)
+        self.node = key.Key.from_text(self.extended_key)
+        if self.node.is_private():
+            print("is_private")
+            self.privatekey_wif = self.node.wif()
+        else:
+            raise ValueError("Your Bip32_Node is not derived from an xprv")
 
-    @classmethod
-    def to_hd_master_key(self, seed, network):
-        if len(seed) != 64:
-            raise ValueError("Provided seed should have length of 64")
-
-        # Compute HMAC-SHA512 of seed
-        seed = hmac.new(b"Bitcoin seed", seed, digestmod=hashlib.sha512).digest()
-
-        # Serialization format can be found at: https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#Serialization_format
-        if network == 'main':
-            xprv = b"\x04\x88\xad\xe4"  # Version for private mainnet
-        elif network == 'test':
-            xprv = b"\x04\x35\x83\x94"  # Version for private testnet
-        xprv += b"\x00" * 9  # Depth, parent fingerprint, and child number
-        xprv += seed[32:]  # Chain code
-        xprv += b"\x00" + seed[:32]  # Master key
-
-        # Double hash using SHA256
-        hashed_xprv = hashlib.sha256(xprv).digest()
-        hashed_xprv = hashlib.sha256(hashed_xprv).digest()
-
-        # Append 4 bytes of checksum
-        xprv += hashed_xprv[:4]
 
     @classmethod
     def b58encode(self, v):
@@ -56,3 +39,28 @@ class Bip39Mnemonic(object):
             acc, idx = divmod(acc, 58)
             string = alphabet[idx : idx + 1] + string
         return string
+
+    @classmethod
+    def to_hd_master_key(self, seed, testnet=False):
+        if len(seed) != 64:
+            raise ValueError("Provided seed should have length of 64")
+
+        # Compute HMAC-SHA512 of seed
+        seed = hmac.new(b"Bitcoin seed", seed, digestmod=hashlib.sha512).digest()
+
+        # Serialization format can be found at: https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#Serialization_format
+        xprv = b"\x04\x88\xad\xe4"  # Version for private mainnet
+        if testnet:
+            xprv = b"\x04\x35\x83\x94"  # Version for private testnet
+        xprv += b"\x00" * 9  # Depth, parent fingerprint, and child number
+        xprv += seed[32:]  # Chain code
+        xprv += b"\x00" + seed[:32]  # Master key
+
+        # Double hash using SHA256
+        hashed_xprv = hashlib.sha256(xprv).digest()
+        hashed_xprv = hashlib.sha256(hashed_xprv).digest()
+
+        # Append 4 bytes of checksum
+        xprv += hashed_xprv[:4]
+
+        return self.b58encode(xprv)
