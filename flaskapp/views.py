ccf28bffaca0b688a61f574d2f4434452f27e88d
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-03-25 00:04:29 +0900

    mod

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -250,7 +250,7 @@ def get_transactions_datalist(txids):
     try:
         url = "https://api.whatsonchain.com/v1/bsv/test/txs"
         headers = {"content-type": "application/json"}
-        json_data = json.dumps({"txids" : txids})
+        json_data = json.dumps({"txids" : ["2a566f32c51227d56ba9bdebe42d9857aeeb50cff074fc553c0d08bc250e0f7c"]]})
         print(json_data)
 
         r = requests.post(url, json_data, headers=headers)
@@ -260,25 +260,27 @@ def get_transactions_datalist(txids):
             op_return = data[i]['vout'][0]['scriptPubKey']['opReturn']
             upload_data = data[i]['vout'][0]['scriptPubKey']['asm'].split()[3] ##uploaddata (charactor)
             if op_return != None:
-                upload_mimetype = op_return['parts'][1] ##MEDIA_Type:  image/png, image/jpeg, text/plain, text/html, text/css, text/javascript, application/pdf, audio/mp3
-                upload_charset = op_return['parts'][2] ##ENCODING: binary, utf-8 (Definition polyglot/upload.py)
-                upload_filename = op_return['parts'][3] ##filename
-                print("upload_mimetype: " + upload_mimetype)
-                print("upload_charset: " + upload_charset)
-                print("upload_filename: " + upload_filename)
-                response = make_response()
-                if upload_charset == 'binary':  #47f0706cdef805761a975d4af2a418c45580d21d4d653e8410537a3de1b1aa4b
-                    #print(binascii.hexlify(upload_data))
-                    response.data = binascii.unhexlify(upload_data)
-                elif upload_charset == 'utf-8':  #cc80675a9a64db116c004b79d22756d824b16d485990a7dfdf46d4a183b752b2
-                    response.data = op_return['parts'][0]
-                else:
-                    print('upload_charset' + upload_charset)
-                    response.data = ''
-                downloadFilename = upload_filename
-                response.headers["Content-Disposition"] = 'attachment; filename=' + downloadFilename
-                response.mimetype = upload_mimetype
-                print(response.data)
+                parts = op_return['parts']
+                if parts != None:
+                    upload_mimetype = parts[1] ##MEDIA_Type:  image/png, image/jpeg, text/plain, text/html, text/css, text/javascript, application/pdf, audio/mp3
+                    upload_charset = parts[2] ##ENCODING: binary, utf-8 (Definition polyglot/upload.py)
+                    upload_filename = parts[3] ##filename
+                    print("upload_mimetype: " + upload_mimetype)
+                    print("upload_charset: " + upload_charset)
+                    print("upload_filename: " + upload_filename)
+                    response = make_response()
+                    if upload_charset == 'binary':  #47f0706cdef805761a975d4af2a418c45580d21d4d653e8410537a3de1b1aa4b
+                        #print(binascii.hexlify(upload_data))
+                        response.data = binascii.unhexlify(upload_data)
+                    elif upload_charset == 'utf-8':  #cc80675a9a64db116c004b79d22756d824b16d485990a7dfdf46d4a183b752b2
+                        response.data = op_return['parts'][0]
+                    else:
+                        print('upload_charset' + upload_charset)
+                        response.data = ''
+                    downloadFilename = upload_filename
+                    response.headers["Content-Disposition"] = 'attachment; filename=' + downloadFilename
+                    response.mimetype = upload_mimetype
+                    print(response.data)
         return response
     except Exception as e:
         print(e)
