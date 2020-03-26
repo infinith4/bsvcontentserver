f2cd934063b4c46b1dc1b1ddfbf8a2dcc0f6e704
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-03-27 00:38:35 +0900

    指定値修正

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -176,21 +176,23 @@ def note(qaddr=''):
             network_api = bitsv.network.NetworkAPI(network='test')
             transactions = network_api.get_transactions(qaddr)
             res_get_textdata = []
-            initindex = 30
-            maxtakecount = 5
+            startindex = 0
+            maxtakecount = 20  ##5 items
+            print(len(transactions))
             #処理前の時刻
             t1 = time.time()
             #for i in range(0, len(transactions), maxtakecount):
-            for i in range(initindex, maxtakecount+initindex):
-                txs = transactions[i:maxtakecount+i]
-                p = multiprocessing.Pool(4) # プロセス数を4に設定
-                result = p.map(get_textdata, txs)
-                #print(result)
-                for item in result:
-                    #print("item")
-                    if item is not None and item.mimetype == "text/plain":
-                        #print(item.data)
-                        res_get_textdata.append(item.data)
+
+            txs = transactions[startindex:maxtakecount+startindex]
+            print(txs)
+            p = multiprocessing.Pool(6) # プロセス数を6に設定
+            result = p.map(get_textdata, txs)  ## arg must be array
+            #print(result)
+            for item in result:
+                #print("item")
+                if item is not None and item.mimetype == "text/plain":
+                    #print(item.data)
+                    res_get_textdata.append(item.data)
             print(len(res_get_textdata))
 
             
@@ -275,6 +277,56 @@ def note(qaddr=''):
     except Exception as e:
         print(e)
 
+class ResponseTx:
+	def __init__(self, data, mimetype, charset, filename):
+		self.data = data
+		self.mimetype = mimetype
+		self.charset = charset
+		self.filename = filename
+
+def get_textdata(txid):
+    try:
+        #print("txid")
+        #print(txid)
+        #time.sleep(0.1)
+        if txid != "":
+            url = "https://api.whatsonchain.com/v1/bsv/test/tx/hash/" + txid
+            headers = {"content-type": "application/json"}
+            r = requests.get(url, headers=headers)
+            data = r.json()
+            op_return = data['vout'][0]['scriptPubKey']['opReturn']
+            if op_return is None:
+                return None
+            hex_upload_data = data['vout'][0]['scriptPubKey']['asm'].split()[3] ##uploaddata (charactor)
+            parts = op_return['parts']
+            if parts is None:
+                return None
+            upload_mimetype = parts[1] ##MEDIA_Type:  image/png, image/jpeg, text/plain, text/html, text/css, text/javascript, application/pdf, audio/mp3
+            upload_charset = parts[2] ##ENCODING: binary, utf-8 (Definition polyglot/upload.py)
+            upload_filename = parts[3] ##filename
+            print("upload_mimetype: " + upload_mimetype)
+            print("upload_charset: " + upload_charset)
+            print("upload_filename: " + upload_filename)
+            print("hex_upload_data: " + hex_upload_data)
+            # response = make_response()
+            if upload_charset == 'binary':  #47f0706cdef805761a975d4af2a418c45580d21d4d653e8410537a3de1b1aa4b
+                #print(binascii.hexlify(upload_data))
+                upload_data = binascii.unhexlify(hex_upload_data)
+            elif upload_charset == 'utf-8':  #cc80675a9a64db116c004b79d22756d824b16d485990a7dfdf46d4a183b752b2
+                upload_data = parts[0]
+            else:
+                #print('upload_charset' + upload_charset)
+                upload_data = ''
+            # downloadFilename = upload_filename
+            # response.headers["Content-Disposition"] = 'attachment; filename=' + downloadFilename
+            # response.mimetype = upload_mimetype
+            #print(upload_data)
+            # return response]
+            return ResponseTx(upload_data, upload_mimetype, upload_charset, upload_filename)
+    except Exception as e:
+        print(e)
+
+
 
 def get_transactions_datalist(txids):
     try:
@@ -314,51 +366,5 @@ def get_transactions_datalist(txids):
                 #     response.mimetype = upload_mimetype
                 #     print(response.data)
         return None
-    except Exception as e:
-        print(e)
-
-class ResponseTx:
-	def __init__(self, data, mimetype, charset, filename):
-		self.data = data
-		self.mimetype = mimetype
-		self.charset = charset
-		self.filename = filename
-
-def get_textdata(txid):
-    try:
-        #print("txid")
-        #print(txid)
-        #time.sleep(0.1)
-        if txid != "":
-            url = "https://api.whatsonchain.com/v1/bsv/test/tx/hash/" + txid
-            headers = {"content-type": "application/json"}
-            r = requests.get(url, headers=headers)
-            data = r.json()
-            op_return = data['vout'][0]['scriptPubKey']['opReturn']
-            if op_return is None:
-                return None
-            hex_upload_data = data['vout'][0]['scriptPubKey']['asm'].split()[3] ##uploaddata (charactor)
-            upload_mimetype = op_return['parts'][1] ##MEDIA_Type:  image/png, image/jpeg, text/plain, text/html, text/css, text/javascript, application/pdf, audio/mp3
-            upload_charset = op_return['parts'][2] ##ENCODING: binary, utf-8 (Definition polyglot/upload.py)
-            upload_filename = op_return['parts'][3] ##filename
-            # print("upload_mimetype: " + upload_mimetype)
-            # print("upload_charset: " + upload_charset)
-            # print("upload_filename: " + upload_filename)
-            # print("hex_upload_data: " + hex_upload_data)
-            # response = make_response()
-            if upload_charset == 'binary':  #47f0706cdef805761a975d4af2a418c45580d21d4d653e8410537a3de1b1aa4b
-                #print(binascii.hexlify(upload_data))
-                upload_data = binascii.unhexlify(hex_upload_data)
-            elif upload_charset == 'utf-8':  #cc80675a9a64db116c004b79d22756d824b16d485990a7dfdf46d4a183b752b2
-                upload_data = op_return['parts'][0]
-            else:
-                #print('upload_charset' + upload_charset)
-                upload_data = ''
-            # downloadFilename = upload_filename
-            # response.headers["Content-Disposition"] = 'attachment; filename=' + downloadFilename
-            # response.mimetype = upload_mimetype
-            #print(upload_data)
-            # return response]
-            return ResponseTx(upload_data, upload_mimetype, upload_charset, upload_filename)
     except Exception as e:
         print(e)
