cc941e9bf0f8a8ed314744922cd298e4c6cc2329
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-04-13 22:14:59 +0900

    mod

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -11,9 +11,10 @@ class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         print("test")
 
-    def test_download_path():
+    def test_download_path(self):
         path = '/download/47f0706cdef805761a975d4af2a418c45580d21d4d653e8410537a3de1b1aa4b'
         app.test_request_context(path)
         assert flask.request.path == path
+
 if __name__ == '__main__':
     unittest.main()
