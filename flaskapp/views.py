75617655553bf48ea203056fdbb0b00735aa746a
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-05-03 19:12:15 +0900

    replace

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -17,6 +17,8 @@ import time
 
 from flaskapp.lib.whats_on_chain_lib import WhatsOnChainLib
 
+api_base_url = "https://bnoteapi.herokuapp.com/v1"
+
 # import logging
 # logging.basicConfig(filename='example.log',level=logging.DEBUG)
 # logger = logging.getLogger(__name__)
@@ -45,30 +47,19 @@ def download(qtxid=''):
             return html
         elif request.method == "POST":
             txid = request.form["transaction"]
-            url = "https://api.whatsonchain.com/v1/bsv/test/tx/hash/" + txid
-            headers = {"content-type": "application/json"}
+            #http://bnoteapi.herokuapp.com/v1/ui/
+            # /api/download/{txid}
+
+            url = api_base_url + "/api/download/" + txid
+            headers = {"content-type": "application/json", "x-api-key": "apikey0"}
             r = requests.get(url, headers=headers)
-            data = r.json()
-            op_return = data['vout'][0]['scriptPubKey']['opReturn']
-            upload_data = data['vout'][0]['scriptPubKey']['asm'].split()[3] ##uploaddata (charactor)
-            upload_mimetype = op_return['parts'][1] ##MEDIA_Type:  image/png, image/jpeg, text/plain, text/html, text/css, text/javascript, application/pdf, audio/mp3
-            upload_charset = op_return['parts'][2] ##ENCODING: binary, utf-8 (Definition polyglot/upload.py)
-            upload_filename = op_return['parts'][3] ##filename
-            print("upload_mimetype: " + upload_mimetype)
-            print("upload_charset: " + upload_charset)
-            print("upload_filename: " + upload_filename)
+            if r.status_code != 200:
+                return None
             response = make_response()
-            if upload_charset == 'binary':  #47f0706cdef805761a975d4af2a418c45580d21d4d653e8410537a3de1b1aa4b
-                #print(binascii.hexlify(upload_data))
-                response.data = binascii.unhexlify(upload_data)
-            elif upload_charset == 'utf-8':  #cc80675a9a64db116c004b79d22756d824b16d485990a7dfdf46d4a183b752b2
-                response.data = op_return['parts'][0]
-            else:
-                print('upload_charset' + upload_charset)
-                response.data = ''
-            downloadFilename = upload_filename
+            response.data = r.content
+            downloadFilename = "filename"
             response.headers["Content-Disposition"] = 'attachment; filename=' + downloadFilename
-            response.mimetype = upload_mimetype
+            #response.mimetype = upload_mimetype
             return response
     except Exception as e:
         print(e)
@@ -81,16 +72,22 @@ def mnemonic():
         return html
     elif request.method == "POST":
         mnemonic = request.form["mnemonic"]  #app.config['TESTNET_MNEMONIC']
-        bip39Mnemonic = Bip39Mnemonic(mnemonic, passphrase="", network="test")
-        privateKey = bitsv.Key(bip39Mnemonic.privatekey_wif, network = 'test')
-        address = privateKey.address
-        balance_satoshi = privateKey.get_balance()
-        balance_bsv = float(balance_satoshi) / float(100000000)
+        #http://bnoteapi.herokuapp.com/v1/ui/
+        # /api/mnemonic
+        url = api_base_url + "/api/mnemonic"
+        headers = {"content-type": "application/json", "x-api-key": "apikey0"}
+        jsondata = {"mnemonic": mnemonic}
+        r = requests.post(url, headers=headers, json=jsondata)
+        if r.status_code != 200:
+            return None
+        response_json = r.json()
+        balance_satoshi = response_json['balance_satoshi']
+        balance_bsv = int(balance_satoshi) / 100000000
         html = render_template(
             'mnemonic.html',
-            privatekey_wif = bip39Mnemonic.privatekey_wif,
-            address = address,
-            balance_satoshi = balance_satoshi,
+            privatekey_wif = response_json["privatekey_wif"],
+            address = response_json["address"],
+            balance_satoshi = response_json["balance_satoshi"],
             balance_bsv = balance_bsv,
             title="mnemonic")
         return html
